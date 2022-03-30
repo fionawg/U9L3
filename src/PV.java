9670314d18cabd3d44eb03527cdea208e78cdd1f
fionawg <fionaw9@nycstudents.net>
2022-03-29 21:01:57 -0400

    commit

--- src/PV.java
+++ src/PV.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 public class PV extends LVM{
     private HD hardDrive;
     private boolean inVG;
+    private VG vg;
 
     public PV(String name, HD hardDrive){
         super(name, hardDrive.getSize());
         this.hardDrive = hardDrive;
         inVG = false;
+        vg = null;
+    }
+
+    public void inVG(VG vg){
+        this.vg = vg;
     }
 
     public boolean getInVG(){
